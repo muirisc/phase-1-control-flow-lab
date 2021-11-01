06b2f2f076d33916f38d1ba5c5bc1ace4bbfb6a1
muirisc <91299876+muirisc@users.noreply.github.com>
2021-11-01 13:28:39 -0400

    commit tree 3569cb6af000000d399623ab0fb490f8cb853d0b

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let result
+  if (distance <= 400) {
+    result = "This one is on me!"
+  }
+  else if (distance > 2000 && distance < 2500) {
+    result = "I will gladly take your thirty bucks."
+  }
+  else result = "No can do."
+return result
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(Location){
+  return (Location == "NYC" ? "Ok, sounds good." : "No go.")
   // Write your code here!
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case 'generous': 
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return "Bye."
+}
 }
